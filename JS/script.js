a08fb203d8adbf9ef98805a349c600411ad99ad0
sibinandhan <ssibinandhan@gmail.com>
2026-06-04 15:31:34 +0530

    css completed

---
+++ JS/script.js
@@ -0,0 +1,84 @@
+// var a = 10; //Reinitialization and Redeclaration 
+// let b = 20; //Reinitialization 
+// const c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var number = 10;
+// var str = "sibi";
+// var undefine;
+// var Null = null;
+// var boolean = true;
+
+// console.log(typeof number);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+// console.log(typeof boolean);
+
+// var bigint = 123n;
+// var symbol = Symbol('li');
+
+// console.log(typeof bigint);
+// console.log(typeof symbol);
+
+// var arr =[10,20,30,40];
+// console.log(typeof arr, arr);
+// var obj = {
+//     name: "sibi",
+//     dept:["ct","aids"]
+// }
+// console.log(typeof obj, obj);
+
+// Arithematic Operators (+,-,*,/,%)
+// var a = 10;
+// var b = "vicky";
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// // logical operators (&&, ||, !)
+// var a = true;
+// var b = false;
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+
+// // Relational Operators (>, <, >=, <=, ==, !=, ===, !==)
+
+// var a = 10;
+// var b = 10;
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+
+// unary operators (++, --)
+var a = 10;
+
+console.log(a++);
+console.log(++a);
+
+var a = 10;
+
+console.log(a--);
+console.log(--a);
+
+a = 1;
+b = 1;
+c = 0;
+
+var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+console.log(result);
+
